b700ebada781a306604408da69417c7ba7c1ff7d
Dranzer <cared@DRANZA.COM>
2022-03-30 17:39:09 -0500

    change image

--- snkinsta/posts/views.py
+++ snkinsta/posts/views.py
@@ -1,33 +1,44 @@
+
+"""Posts views."""
 # Django
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Utilities
 from datetime import datetime
 
-# Create your views here.
+
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'declaration of war',
+        'user': {
+            'name': 'Eren Jaeger',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://animenewsandfacts.com/wp-content/uploads/2020/12/Attack-On-Titan-Season-4-Episode-5-Countdown.jpg',
     },
     {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
+        'title': 'sea ',
+        'user': {
+            'name': 'Mikasa Akerman',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://www.tonica.la/__export/1620830185141/sites/debate/img/2021/05/12/mikasa-ackerman-se-quedx-con-jean.jpg_1902800913.jpg',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
+        'title': 'beaten the beast ',
+        'user': {
+            'name': 'Levi Akerman',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://pbs.twimg.com/media/EceJXtZWsAAHRsm.jpg',
     }
 ]
 
 
 def list_posts(request):
     """List existing posts."""
-    return render(request,'posts/feed.html')
+    return render(request, 'posts/feed.html', {'posts': posts})
