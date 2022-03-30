d0aedae78ed8c49e2be046e206ea6999f5f42324
Dranzer <cared@DRANZA.COM>
2022-03-29 19:34:58 -0500

    views-snk start

--- snkinsta/posts/views.py
+++ snkinsta/posts/views.py
@@ -1,3 +1,33 @@
+# Django
+from django.http import HttpResponse
 from django.shortcuts import render
 
+# Utilities
+from datetime import datetime
+
 # Create your views here.
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+
+def list_posts(request):
+    """List existing posts."""
+    return render(request,'posts/feed.html')
