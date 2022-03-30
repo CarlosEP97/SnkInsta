8ffcfe1fedb95b63df52c3d04ee79a99e3f83f9d
Dranzer <cared@DRANZA.COM>
2022-03-29 19:35:17 -0500

    add posts-app start

--- snkinsta/snkinsta/settings.py
+++ snkinsta/snkinsta/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#se definen todas las apps que funcionan en nuestro proyecto
+
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
