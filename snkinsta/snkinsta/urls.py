9fe5eb8441dd6298f47084318799018e3bebe330
Dranzer <cared@DRANZA.COM>
2022-03-31 16:55:02 -0500

    adding admin

--- snkinsta/snkinsta/urls.py
+++ snkinsta/snkinsta/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
     path('hello-world/', views.hello_world),
     path('sorted/', views.sorted_integers),
     path('hi/<str:name>/<int:age>', views.say_hi),
