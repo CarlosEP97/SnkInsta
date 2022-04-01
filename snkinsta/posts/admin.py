56106f432c1da31b775e9b8ad2fff5a4b9ed3302
Dranzer <cared@DRANZA.COM>
2022-04-01 16:28:49 -0500

    post model and admin + name correction

--- snkinsta/posts/admin.py
+++ snkinsta/posts/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
 
 # Register your models here.
+
+"""Posts admin classes"""
+#Django
+from django.contrib import admin
+
+#Models
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('pk', 'user', 'title','photo',)
+  list_display_links = ('pk', 'user',)
+  list_editable = ('title', 'photo',)
+  search_fields = ('user__username','title',)
+
+  readonly_fields = ('created', 'modified')
+
+  list_filter = (
+      'title',
+      'created'
+  )
+
