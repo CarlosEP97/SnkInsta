45ad8d66e545d51c96fea68bb529bd0600e7ddb5
Dranzer <cared@DRANZA.COM>
2022-04-22 15:28:46 -0500

    template view

--- snkinsta/users/urls.py
+++ snkinsta/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'users'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout',views.logout_view, name = 'logout'),
     path('signup',views.signup, name = 'signup'),
-    path('me/profile',views.update_profile, name='update_profile')
+    path('me/profile',views.update_profile, name='update_profile'),
+    path('<str:username>', TemplateView.as_view(template_name='users/detail.html'), name='detail'),
 
 ]
