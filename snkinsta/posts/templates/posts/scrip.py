7fe7d42f79eb701c79ea905f61bb4db05c57b853
Dranzer <cared@DRANZA.COM>
2022-04-26 03:09:38 -0500

    scrip

--- snkinsta/posts/templates/posts/scrip.py
+++ snkinsta/posts/templates/posts/scrip.py
@@ -1,43 +1,42 @@
-from datetime import date
-
-users = [
-    {
-        'email': 'Dranza@gmail.com',
-        'first_name': 'Carlos',
-        'last_name': 'Parra',
-        'password': 'Dranza997',
-        'is_admin': True
-    },
-    {
-        'email': 'leviAk@gmail.com',
-        'first_name': 'Levi',
-        'last_name': 'Ackerman',
-        'password': 'Lev1854',
-        'birthdate': date(829, 12,25)
-
-    },
-    {
-        'email': 'Mikasa@gmail.com',
-        'first_name': 'Mikasa',
-        'last_name': 'Ackerman',
-        'password': 'mikasa854',
-        'birthdate': date(845, 2,10)
-    },
-    {
-        'email': 'tatakae@gmail.com',
-        'first_name': 'Eren',
-        'last_name': 'Jaeger',
-        'password': 'tatakae30',
-        'is_admin': True,
-        'birthdate': date(845, 3, 30),
-        'bio': "me llamo eren jaeger,uso el poder del titan fundador para hablar con el pueblo de ymir."
-
-    }
-]
-
-from posts.models import User
-
-for user in users:
-    obj = User(**user)
-    obj.save()
-    print(obj.pk, ':', obj.email)
+# from datetime import date
+#
+# users = [
+#     {
+#         'email': 'Dranza@gmail.com',
+#         'first_name': 'Carlos',
+#         'last_name': 'Parra',
+#         'password': 'Dranza997',
+#         'is_admin': True
+#     },
+#     {
+#         'email': 'leviAk@gmail.com',
+#         'first_name': 'Levi',
+#         'last_name': 'Ackerman',
+#         'password': 'Lev1854',
+#
+#
+#     },
+#     {
+#         'email': 'Mikasa@gmail.com',
+#         'first_name': 'Mikasa',
+#         'last_name': 'Ackerman',
+#         'password': 'mikasa854',
+#
+#     },
+#     {
+#         'email': 'tatakae@gmail.com',
+#         'first_name': 'Eren',
+#         'last_name': 'Jaeger',
+#         'password': 'tatakae30',
+#         'is_admin': True,
+#
+#
+#     }
+# ]
+#
+# from posts.models import User
+#
+# for user in users:
+#     obj = User(**user)
+#     obj.save()
+#     print(obj.pk, ':', obj.email)
